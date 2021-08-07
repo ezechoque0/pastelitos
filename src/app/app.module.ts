ea879eaaf49fce8951d2dedb6f2318c4e6709265
EZEQUIEL-CHOQUE <ezequielchoque0@gmail.com>
2021-08-07 10:43:19 -0300

    4Pag y 2com(nave,footer),decla en app.module.ts😁

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaveComponent } from './components/nave/nave.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioTiendaNosotrosContactoComponent } from './pages/inicio-tienda-nosotros-contacto/inicio-tienda-nosotros-contacto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaveComponent,
+    FooterComponent,
+    InicioTiendaNosotrosContactoComponent,
+    InicioComponent,
+    TiendaComponent,
+    NosotrosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
